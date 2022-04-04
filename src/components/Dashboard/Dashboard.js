675d18a4b320c87670f42429bd23e8116bd9bc34
MK-Sohan <mksohan786@gmail.com>
2022-04-04 12:13:13 +0600

    eight

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -57,6 +57,32 @@ const Dashboard = () => {
           <Line type="monotone" dataKey="revenue" stroke="#82ca9d" />
         </LineChart>
       </div>
+      <div className="third-chart">
+        <LineChart
+          width={800}
+          height={600}
+          data={charts}
+          margin={{
+            top: 5,
+            right: 30,
+            left: 20,
+            bottom: 5,
+          }}
+        >
+          <CartesianGrid strokeDasharray="3 3" />
+          <XAxis dataKey="month" />
+          <YAxis dataKey="investment" />
+          <Tooltip />
+          <Legend />
+          <Line
+            type="monotone"
+            dataKey="revenue"
+            stroke="#8884d8"
+            activeDot={{ r: 8 }}
+          />
+          <Line type="monotone" dataKey="sell" stroke="#82ca9d" />
+        </LineChart>
+      </div>
     </div>
   );
 };
